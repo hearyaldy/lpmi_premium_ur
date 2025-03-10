b6945d529a749ba580ddcdb7ac8438813c73f08f
hearyaldy <heary_aldy@hotmail.com>
2025-03-10 09:47:36 +0700

    Still fixing

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
         classpath("com.google.gms:google-services:4.4.1")
     }
 }
